f14c4326cc2cdd60ee2af274b9b1cbb71b4541ca
falbuquerque <albuquerque.felipe@gmail.com>
2015-03-18 07:49:15 -0300

    Fixing the TimeServer endpoint

--- src/main/java/falbuquerque/study/ws/timer/TimeServerPublisher.java
+++ src/main/java/falbuquerque/study/ws/timer/TimeServerPublisher.java
@@ -5,14 +5,12 @@ import java.util.concurrent.Executors;
 
 import javax.xml.ws.Endpoint;
 
-import falbuquerque.study.ws.teams.TeamsSOAPImpl;
-
 class TimeServerPublisher {
 
     private static final ExecutorService executor = Executors.newWorkStealingPool();
     
     public static void main(String[] args) {
-        final Endpoint endpoint = Endpoint.create(new TeamsSOAPImpl());
+        final Endpoint endpoint = Endpoint.create(new TimeServerSOAPImpl());
         endpoint.setExecutor(executor);
         endpoint.publish("http://127.0.0.1:9877/ts");
     }
