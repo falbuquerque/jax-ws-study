1355c7cbe7069372bd10f422683264417bc226cd
falbuquerque <albuquerque.felipe@gmail.com>
2015-03-18 09:26:40 -0300

    TimeServer with document-style binding

--- src/main/java/falbuquerque/study/ws/timer/TimeServer.java
+++ src/main/java/falbuquerque/study/ws/timer/TimeServer.java
@@ -3,11 +3,8 @@ package falbuquerque.study.ws.timer;
 import javax.jws.WebMethod;
 import javax.jws.WebResult;
 import javax.jws.WebService;
-import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;
 
 @WebService
-@SOAPBinding(style = Style.RPC)
 public interface TimeServer {
 
     @WebMethod
--- src/main/java/falbuquerque/study/ws/timer/TimeServerPublisher.java
+++ src/main/java/falbuquerque/study/ws/timer/TimeServerPublisher.java
@@ -8,11 +8,11 @@ import javax.xml.ws.Endpoint;
 class TimeServerPublisher {
 
     private static final ExecutorService executor = Executors.newWorkStealingPool();
-    
+
     public static void main(String[] args) {
         final Endpoint endpoint = Endpoint.create(new TimeServerSOAPImpl());
         endpoint.setExecutor(executor);
         endpoint.publish("http://127.0.0.1:9877/ts");
     }
-    
+
 }
