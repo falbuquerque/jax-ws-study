12cb1bb630252cae7405855219302a3bc3e21fc0
falbuquerque <albuquerque.felipe@gmail.com>
2015-03-19 09:09:02 -0300

    SOAP Handlers

---
+++ src/main/java/falbuquerque/study/ws/handler/UUIDHandler.java
@@ -0,0 +1,77 @@
+package falbuquerque.study.ws.handler;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.UUID;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPEnvelope;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPHeaderElement;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+
+public class UUIDHandler implements SOAPHandler<SOAPMessageContext> {
+
+    private final Logger logger = LogManager.getLogManager().getLogger("ClientSideLogger");
+
+    @Override
+    public boolean handleMessage(final SOAPMessageContext context) {
+        logger.info("message");
+
+        final Boolean messageOutgoing = (Boolean) context.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+
+        if ((messageOutgoing != null) && messageOutgoing) {
+
+            try {
+                final SOAPMessage message = context.getMessage();
+                final SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
+                final SOAPHeader header = (envelope.getHeader() == null) ? envelope.addHeader() : envelope.getHeader();
+
+                final QName uuidHeader = new QName("http://ws.handler.study.falbuquerque/", "uuid");
+                final SOAPHeaderElement uuidHeaderElement = header.addHeaderElement(uuidHeader);
+                uuidHeaderElement.setActor(SOAPConstants.URI_SOAP_ACTOR_NEXT);
+                uuidHeaderElement.addTextNode(UUID.randomUUID().toString());
+
+                message.saveChanges();
+                message.writeTo(System.out);
+            } catch (final SOAPException | IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return true; // continue down the chain
+    }
+
+    @Override
+    public boolean handleFault(final SOAPMessageContext context) {
+        logger.info("fault");
+
+        try {
+            context.getMessage().writeTo(System.out);
+        } catch (final SOAPException | IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    @Override
+    public void close(final MessageContext context) {
+        logger.info("close");
+    }
+
+    @Override
+    public Set<QName> getHeaders() {
+        logger.info("headers");
+        return null;
+    }
+
+}
